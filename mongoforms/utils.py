9a3cfd4ca2500f5ae581046722bd3f182308aea1
Nikita Frolenkov <konung.nikita@gmail.com>
2011-02-01 12:52:42 +0500

    Fixed NameError in utils.py.

--- mongoforms/utils.py
+++ mongoforms/utils.py
@@ -1,8 +1,11 @@
+from django import forms
+
 from mongoengine.base import ValidationError
+
 
 def mongoengine_validate_wrapper(old_clean, new_clean):
     """
-    A wrapper function to validate formdata against mongoengine-field 
+    A wrapper function to validate formdata against mongoengine-field
     validator and raise a proper django.forms ValidationError if there
     are any problems.
     """
@@ -16,15 +19,18 @@ def mongoengine_validate_wrapper(old_clean, new_clean):
             raise forms.ValidationError(e)
     return inner_validate
 
+
 def iter_valid_fields(meta):
     """walk through the available valid fields.."""
 
     # fetch field configuration and always add the id_field as exclude
     meta_fields = getattr(meta, 'fields', ())
-    meta_exclude = getattr(meta, 'exclude', ()) + (meta.document._meta.get('id_field'),)
+    meta_exclude = getattr(meta, 'exclude', ()) \
+                   + (meta.document._meta.get('id_field'),)
     # walk through the document fields
     for field_name, field in meta.document._fields.iteritems():
         # skip excluded or not explicit included fields
-        if (meta_fields and field_name not in meta_fields) or field_name in meta_exclude:
+        if (meta_fields and field_name not in meta_fields) \
+               or field_name in meta_exclude:
             continue
         yield (field_name, field)
