eae600dfd35346f5e81e3004d17a294e80733d35
Nikita Frolenkov <konung.nikita@gmail.com>
2011-02-02 11:45:31 +0500

    If Meta.fields is set then fields on form will follow the same order.

--- mongoforms/utils.py
+++ mongoforms/utils.py
@@ -28,9 +28,18 @@ def iter_valid_fields(meta):
     meta_exclude = getattr(meta, 'exclude', ()) \
                    + (meta.document._meta.get('id_field'),)
     # walk through the document fields
+    valid_fields = []
     for field_name, field in meta.document._fields.iteritems():
         # skip excluded or not explicit included fields
         if (meta_fields and field_name not in meta_fields) \
                or field_name in meta_exclude:
             continue
-        yield (field_name, field)
+        valid_fields.append((field_name, field))
+
+    # use sorting order provided by Meta.fields
+    if meta_fields:
+        valid_fields = sorted(valid_fields, lambda x, y: cmp(meta_fields.index(x),
+                                          meta_fields.index(y)))
+
+    for field in valid_fields:
+        yield field
